bb9604cfcbf2acba39121adb6e989d7be913a9c6
lfe9142 <lflippo001@gmail.com>
2018-09-19 23:04:44 -0500

    added tab funcationality

--- src/App.js
+++ src/App.js
@@ -5,11 +5,53 @@ import BioComponent from './BioComponent';
 import LinkButtonComponent from './LinkButtonComponent';
 
 class App extends Component {
+  state = {
+    tab: 1,
+  }
+
+  handleTabButtonClick(newTab) {
+      this.setState({tab: newTab});
+  }
+
+  showCurrentTab() {
+    const currentTab = this.state.tab;
+    switch (currentTab) {
+        case 1:
+            return (
+                <div>
+                  <BioComponent/>
+                </div>
+            )
+        case 2:
+            return (
+                <div>
+                    These are my skills!
+                </div>
+            )   
+        case 3:
+            return (
+                <div>
+                    You can contact me here!
+                </div>
+            )
+    }
+}
+
+
   render() {
     return (
       <div className="App">
        <h1>Levi Flippo</h1>
-       <BioComponent/>
+       <div>
+                 <div>
+                    <button onClick={() => this.handleTabButtonClick(1)}>About</button>
+                    <button onClick={() => this.handleTabButtonClick(2)}>Skills</button>
+                    <button onClick={() => this.handleTabButtonClick(3)}>Contact</button>
+                </div>
+
+                <div>{this.showCurrentTab()}</div>
+            </div>
+       
        <LinkButtonComponent name="Github" link="https://github.com/lfe9142"/>
        <LinkButtonComponent name="LinkedIn" link="https://www.linkedin.com/in/levi-flippo-7443b6145/"/>
 
--- src/BioComponent.js
+++ src/BioComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Button from '@material-ui/core/Button';
+
 
 class BioComponent extends Component {
     render() {
